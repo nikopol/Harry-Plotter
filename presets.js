15c83d7ccc398d7548bc887fee3eff4a23189678
niko <nikomomo@gmail.com>
2012-09-21 14:31:09 +0200

    samples added

--- presets.js
+++ presets.js
@@ -1,18 +1,77 @@
 {
-	"Dark River": {
-		"datas":[
-			{"values":[51,52,47,6,5,86,95,93,96,22,55,49,21,21,6,49],"color":"#fc0"},
-			{"values":[59,21,30,86,5,75,24,92,80,11,22,83,19,91,99,74],"color":"#0cf"},
-			{"values":[58,0,5,40,82,37,37,77,92,76,38,47,98,43,38,42]}
-		],
-		"title":{"text":"DARK RIVER","font":"bold 24px \"Sans Serif\"","color":"#fff","shadow":"0,0,8,#fff"},
-		"labels":{"x":1,"y":[0,50,100],"color":"#ddd","ypos":"right"},
-		"background":"#000",
-		"mode":"curve:river",
-		"fill":"solid",
-		"opacity":0.8,
-		"linewidth":3,
-		"autoscale":"top",
-		"legends":{"background":"#666","color":"#fff","shadowbox":"1,1,1,#fff"}
+	"Lines & Curves": {
+		"Tiny Light Filled": {
+			"datas":[{"values":[69,89,19,29,58,90,12,73,11,91,33]}],
+			"grid":{"x":"none","y":"none"},
+			"width":30,
+			"height":15,
+			"mode":"line",
+			"fill":"solid",
+			"legends":false,
+			"mouseover":false
+		},
+		"Filled River Lines": {
+			"datas": [
+				{"title":"batman","values":[51,52,47,6,5,86,95,93,96,22,55,49,21,21,6,49]},
+				{"title":"joker","values":[59,21,30,86,5,75,24,92,80,11,22,83,19,91,99,74]}
+			],
+			"labels":{"color":"#fff","ypos":"left","x":3,"y":[50,100],"marks":2},
+			"width":200,
+			"height":100,
+			"mode":"line:river",
+			"fill":"solid",
+			"autoscale":"top",
+			"mouseover":{"radius":4,"linewidth":2,"circle":"#fff"},
+			"legends":{"color":"#fff","shadow":"1,1,0,#000"}
+		},
+		"Dark River": {
+			"datas": [
+				{"values":[51,52,47,6,5,86,95,93,96,22,55,49,21,21,6,49],"color":"#fc0"},
+				{"values":[59,21,30,86,5,75,24,92,80,11,22,83,19,91,99,74],"color":"#0cf"},
+				{"values":[58,0,5,40,82,37,37,77,92,76,38,47,98,43,38,42]}
+			],
+			"title":{"text":"DARK RIVER","font":"bold 24px \"Sans Serif\"","color":"#fff","shadow":"0,0,8,#fff"},
+			"labels":{"x":1,"y":[0,50,100],"color":"#ddd","ypos":"right"},
+			"background":"#000",
+			"mode":"curve:river",
+			"fill":"solid",
+			"opacity":0.8,
+			"linewidth":3,
+			"autoscale":"top",
+			"mouseover":{"radius":4,"linewidth":0,"circle":"#fff"},
+			"legends":{"background":"#444","color":"#fff","border":"#fff"}
+		}
+	},
+	"Pies": {
+		"Simple light": {
+			"datas": [
+				{"title":"yellow","color":"#fc0","values":[51,52,47,6,5,86,95,93,96,22,55,49,21,21,6,49]},
+				{"title":"blue","color":"#0cf","values":[59,21,30,86,5,75,24,92,80,11,22,83,19,91,99,74]},
+				{"title":"green","color":"#0f8","values":[58,0,5,40,82,4,37,8,92,1,38,0,98,1,3,3]}
+			],
+			"width":280,
+			"height":200,
+			"mode":"pie",
+			"margins":[20,20,20,100],
+			"background":"#fff",
+			"mouseover":{"text":"%t: %p"},
+			"legends":{"x":2,"y":2,"font":"bold 16px \"Sans Serif\""}
+		}
+	},
+	"Charts": {
+		"Light": {
+			"datas": [
+				{"title":"in","color":"#393","values":{"May 12":20,"May 13":32,"May 14":55,"May 15":11,"May 16":18}},
+				{"title":"out","color":"#933","values":{"May 12":14,"May 13":21,"May 14":66,"May 15":33,"May 16":15}}
+			],
+			"labels":{"color":"#666","x":1,"y":[0,50,100]},
+			"width":250,
+			"height":100,
+			"mode":"chart",
+			"fill":"vertical",
+			"background":"#fff",
+			"autoscale":"top",
+			"legends":{"color":"#","border":"#fff"}
+		}
 	}
 }
