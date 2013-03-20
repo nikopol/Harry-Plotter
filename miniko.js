ef44102d4ad1417abb4227189e69ac4fa763519f
niko <nikomomo@gmail.com>
2013-03-20 14:40:09 +0100

    anim added

---
+++ miniko.js
@@ -0,0 +1,176 @@
+// miniko.js 0.4
+// ~L~ nikomomo@gmail.com 2012-2013
+// https://github.com/nikopol/miniko.js
+
+/*
+**selectors**
+
+if content is provided, all matching element will have it.
+
+  _(element,[content])    : return the element provided
+  _("#id",[content])      : return the element by id
+  _(".class",[content])   : return an elements array matching a classname
+  _("selector",[content]) : return an elements array matching a selector
+
+**manipulation**
+
+  append(sel,html)        : append html to matching element(s)
+
+**style**  
+
+  css(sel,'class')        : set classname to matching element(s)
+  css(sel,'+class')       : add class to matching element(s)
+  css(sel,'-class')       : remove class to matching element(s)
+  css(sel,'*class')       : toggle class to matching element(s)
+  css(sel,{style:value})  : style's values to matching element(s)
+  
+**geometry**
+
+  position(element)
+  position("#id")         : return {left,top,width,height}
+
+**ajax**
+  
+  ajax(url,ok)            : perform a GET ajax call
+  ajax({                  : perform an ajax call
+     url: '?'           
+     type: 'GET|DELETE|POST|PUT'
+     data: {...},
+     ok: function(data,xhr){},
+     error: function(responsetext,xhr){},
+     datatype: 'application/json',
+     contenttype: 'application/x-www-form-urlencoded',
+     timeout: 30,
+     headers: {
+      'key': value
+     }
+  })
+  
+**events**
+
+  ready(callback) : callback when the dom is ready
+
+*/
+
+"use strict";
+
+var
+
+__ = function(o){ return o instanceof Array ? o : [o]; },
+
+_ = function(s,h){
+	var o,l,n;
+	if(typeof(s)=='object') o = s;
+	else if(s.length) {
+		if(s[0]=='#' && !/[ \.\>\<]/.test(s)) o = document.getElementById(s.substr(1));
+		else {
+			l = document.querySelectorAll(s);
+			for(o=[],n=0; n<l.length; ++n) o.push(l[n]);
+		}
+	}
+	if(o && h!=undefined) __(o).forEach(function(e){ e.innerHTML = h });
+	return o;
+},
+
+append = function(s,h){
+	var o = _(s);
+	if(o)
+		__(o).forEach(function(e){
+			var c = document.createElement('div');
+			c.innerHTML = h;
+			while(c.childNodes.length) e.appendChild(c.childNodes[0]);
+		});
+	return o;
+},
+
+css = function(s,c){
+	var o = _(s),z,l;
+	if(!o) return;
+	if(c==undefined) return o instanceof Array ? o : o.className;
+	if(typeof c=='object')
+		__(o).forEach(function(e){ for(z in c) e.style[z] = c[z] });
+	else if(/^([\+\-\*])(.+)$/.test(c)) {
+		z = RegExp.$1;
+		c = RegExp.$2;
+		__(o).forEach(function(e){
+			l = e.className.split(/\s+/).filter(function(n){return n});
+			if(z!='-' && l.indexOf(c)==-1) l.push(c);  //add class
+			else if(z!='+') l=l.filter(function(n){return n!=c}); //remove class
+			o.className = l.join(' ');
+		});
+	} else
+		__(o).forEach(function(e){ e.className = c });
+	return o;
+},
+
+ajax = function(o,fn){
+	if(typeof(o)=='string') o = { url:o, ok:fn };
+	var
+		app  = 'application/',
+		type = o.type || 'GET',
+		url  = o.url || '',
+		ctyp = o.contenttype || app+'x-www-form-urlencoded',
+		dtyp = o.datatype || app+'json',
+		xhr  = new window.XMLHttpRequest(),
+		timer,d,n;
+	if(o.data){
+		if(typeof(o.data)=='string') d = o.data;
+		else if(/json/.test(ctyp))   d = JSON.stringify(o.data);
+		else {
+			d = [];
+			for(n in o.data)
+				d.push(encodeURIComponent(n)+'='+encodeURIComponent(o.data[n]));
+			d = d.join('&');
+		}
+		if(/GET|DEL/i.test(type)) {
+			url += /\?/.test(url) ? '&'+d : '?'+d;
+			d = '';
+		}
+	}
+	if(!o.error) o.error=function(t,xhr){ console.error(t,xhr) };
+	if(!o.ok)    o.ok=function(){};
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState==4) {
+			if(timer) clearTimeout(timer);
+			if(/^2/.test(xhr.status)) {
+				d=xhr.responseText;
+				if(/json/.test(dtyp)) {
+					try { d = JSON.parse(xhr.responseText) }
+					catch(e) { return o.error('json parse error: '+e.message,xhr) }
+				}
+				o.ok(d,xhr);
+			} else
+				o.error(xhr.responseText,xhr);
+		}
+	};
+	xhr.open(type, url, true);
+	xhr.setRequestHeader('Content-Type', ctyp);
+	if(o.headers) for(n in o.headers) xhr.setRequestHeader(n, o.headers[n]);
+	if(o.timeout) timer = setTimeout(function(){
+		xhr.onreadystatechange = function(){};
+		xhr.abort();
+		if(o.error) o.error('timeout',xhr);
+	}, o.timeout*1000);
+	xhr.send(d);
+	return xhr;
+},
+
+position = function(e){
+	var	o = _(e);
+	if(o) {
+		var r = o.getBoundingClientRect();
+		return {
+			left: r.left+window.pageXOffset,
+			top: r.top+window.pageYOffset,
+			width: r.width,
+			height: r.height
+		};
+	}
+	return false;
+},
+
+ready = function(cb){
+	if(/complete|loaded|interactive/.test(document.readyState)) cb();
+	else if(document.attachEvent) document.attachEvent('ondocumentready',cb()); 
+	else document.addEventListener('DOMContentLoaded',function(){cb()}, false);
+};
---
+++ minitools.js
@@ -0,0 +1,153 @@
+// minitools.js 0.1
+// ~L~ nikomomo@gmail.com 2012
+// https://github.com/nikopol/minitools.js
+
+/*
+minitools.js is a collection of small js helpers for "modern" browsers.
+
+** hash **
+
+manage variables in the url anchor.
+
+  hash.set({page:"home",skin:"default"});  //set the complete anchor
+  hash.set("key","value");                 //set a variable
+  all=hash.get();                          //get all variables in an js object
+  val=hash.get("key");                     //get a variable
+  hash.del("key");                         //remove a variable
+  hash.set({});                            //remove all variables
+  hash.onchange(function(vars){ ... });    //setup a callback called on anchor update
+
+** hotkeys **
+
+handle hotkeys
+
+  hotkeys.add("CTRL+U",function(){})              //define a hotkey
+         .add(["CTRL+Q","ALT+F4"],function(){});  //define 2 equivalents hotkeys
+  hotkeys.clear();                                //unset all hotkeys
+
+
+** browser **
+
+an object with client info
+
+           Firefox 11 => { Firefox:11, Gecko:20100101, Mozilla:5 }
+          Chromium 18 => { Chrome:18, Safari:535.19, Mozilla:5 }
+                 IE 9 => { IE:9, Mozilla:5 }
+                 IE 8 => { IE:8, Mozilla:4 }
+                 IE 7 => { IE:7, Mozilla:4 }
+              Opera 9 => { Opera: 9.8, Presto: 2.1, Version: 11.61 }
+             Safari 5 => { Version:5.1, Safari:534.52, Mozilla:5 }
+
+*/
+
+"use strict";
+
+var
+hash=(function(){
+	var h, p,
+	encode=function(s){ return s.replace(/ /g,'%20').replace(/#/,'%23') },
+	decode=function(s){ return s.replace(/%20/g,' ').replace(/%23/,'#') },
+	serialize=function(){
+		var a=[], k;
+		for (k in h) a.push(k+"="+h[k]);
+		p=a.join("|");
+		document.location.hash="#"+encode(p);
+		return true;
+	},
+	unserialize=function(){
+		h={};
+		p=decode(document.location.hash.substr(1));
+		if(p.length) {
+			var lst=p.split("|");
+			lst.forEach(function(l){
+				var kv=l.split('=');
+				if(kv.length>1) h[kv.shift()] = kv.join('=');
+			});
+		}
+	};
+	unserialize();
+	return {
+		del: function(key){ if(key in h){ delete h[key]; return serialize(h) } return false },
+		set: function(key,val){ return serialize(typeof(key)=='object' ? h = key : h[key] = val) },
+		get: function(key){ return key==undefined ? h : decode(h[key]||'') },
+		onchange: function(cb){
+			window.onhashchange=function(){
+				var q=decode(document.location.hash.substr(1));
+				if(q!=p){
+					unserialize();
+					if(cb) cb();
+				}
+			}
+		}
+	}
+})(),
+
+hotkeys=(function(){
+	var 
+	on=false,
+	KEYS={
+		ESC:27, TAB:9, SPACE:32, RETURN:13, BACKSPACE:8, BS:8, SCROLL:145, CAPSLOCK:20, NUMLOCK:144, PAUSE:19, 
+		INSERT:45, DEL:46, HOME:36, END:35, PAGEUP:33, PAGEDOWN:34, LEFT:37, UP:38, RIGHT:39, DOWN:40,
+		F1:112, F2:113, F3:114, F4:115, F5:116, F6:117, F7:118, F8:119, F9:120, F10:121, F11:122, F12:123
+	},
+	MASKEYS={ ALT:1,CONTROL:2,CTRL:2,SHIFT:4 },
+	list=[],
+	cold=function(){ return /INPUT|SELECT|TEXTAREA/.test(document.activeElement.tagName) },
+	trigger=function(e){
+		if(!e) e=window.event;
+		var i, k,
+			chr=String.fromCharCode(e.which || e.charCode).toUpperCase(),
+			msk=e.shiftKey*MASKEYS.SHIFT|e.ctrlKey*MASKEYS.CTRL|e.altKey*MASKEYS.ALT;
+		for(i in list) {
+			k=list[i];
+			if((e.which==k.key || chr==k.key) && msk==k.mask) {
+				if(k.glob || !cold()) {
+					k.fn(e);
+					e.stopPropagation();
+					e.preventDefault();
+					return false;
+				}
+			}
+		}
+		return true;
+	};
+	return {
+		clear: function() {
+			document.onkeydown=null;
+			on=false;
+			list=[];
+			return this;
+		},
+		add: function(keys,fn,global) { 
+			var
+				mask=0,
+				skey=0,
+				i, j, key, lst, n;
+			if(typeof keys=="string") keys=[keys];
+			keys.forEach(function(key){
+				if(typeof key=="string")
+					key.toUpperCase().split('+').forEach(function(n){
+						if(MASKEYS[n]) mask|=MASKEYS[n];
+						else if(KEYS[n]) skey=KEYS[n];
+						else skey=n[0];
+					});
+				if(skey) {
+					list.push({key:skey, fn:fn, glob:global, mask:mask||0});
+					if(!on){
+						document.onkeydown=trigger;
+						on=true;
+					}
+				} else
+					console.error('hotkey '+key+' unknown');
+			});
+			return this;
+		}
+	};
+})(),
+
+browser=function(){
+	var b={}, z=navigator.userAgent;
+	if(/MSIE\s([\d\.]+)/.test(z)) b.IE=parseFloat(RegExp.$1);
+	z.replace(/\s\(.+\)/g,'').split(' ').forEach(function(n){ if(/^(.+)\/(.+)$/.test(n)) b[RegExp.$1]=parseFloat(RegExp.$2) });
+	return b;
+}();
